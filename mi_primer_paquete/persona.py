bcb9f49a477e539ecc5a6953afc3baa9b4c367b5
Luciano Leone <luciano.leone@gmail.com>
2025-10-21 19:48:21 -0300

    prueba cambios clase

--- mi_primer_paquete/persona.py
+++ mi_primer_paquete/persona.py
@@ -4,4 +4,7 @@ class Persona:
         self.apellido = apellido
     def __str__(self):
         return f"Nombre: {self.nombre} , Apellido: {self.apellido}"
-    def agregar_persona
+    def agregar_persona():
+        nombre = input("Ingrese el nombre: ")
+        apellido = input("Ingrese el apellido: ")
+        return Persona(nombre, apellido)
